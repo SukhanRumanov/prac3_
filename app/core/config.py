6028f00c7c0d75cfed4b35f34b9f9fd37def3482
SukhanRumanov <romas3.11.05@mail.ru>
2025-11-12 12:52:32 +0500

    поправил

--- app/core/config.py
+++ app/core/config.py
@@ -1,12 +1,26 @@
+import os
 from pydantic import BaseModel
+from dotenv import load_dotenv
 
-SECRET_KEY: str = ""
-ALGORITHM: str = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+load_dotenv()
+
+SECRET_KEY: str = os.getenv("SECRET_KEY", "123456")
+ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
+ACCESS_TOKEN_EXPIRE_MINUTES: int = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "30"))
 
 class Settings(BaseModel):
-    DATABASE_URL: str = "postgresql+asyncpg://postgres:@postgres:5432/employee_db"
-    PROJECT_NAME: str = "Employee Management System"
-    DEBUG: bool = True
+    DB_HOST: str = os.getenv("DB_HOST", "localhost")
+    DB_PORT: str = os.getenv("DB_PORT", "5432")
+    DB_NAME: str = os.getenv("DB_NAME", "employee_db")
+    DB_USER: str = os.getenv("DB_USER", "postgres")
+    DB_PASSWORD: str = os.getenv("DB_PASSWORD", "123456")
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    PROJECT_NAME: str = os.getenv("PROJECT_NAME", "Employee Management System")
+    DEBUG: bool = os.getenv("DEBUG", "True").lower() == "true"
+
 
 settings = Settings()
